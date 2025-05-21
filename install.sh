5413681257dec98f44e67c5f7e877a002c7a68f5
Aiden Avery <email>
2025-05-21 17:35:35 -0400

    removed hyprpaper from install

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 # install everything
-sudo pacman -S hyprland alacritty waybar rofi-wayland hyprpaper hyprlock uwsm
+sudo pacman -S hyprland alacritty waybar rofi-wayland hyprlock uwsm
 yay -S linux-wallpaperengine
