a1066771935568df8394d96748cb92f63d8f6c20
MouseUser <118291523+MouseUser@users.noreply.github.com>
2025-06-13 15:00:20 -0400

    add ghostty, grim, slurp to install list

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 # install everything
-sudo pacman -S hyprland alacritty waybar rofi-wayland hyprlock uwsm
+sudo pacman -S hyprland ghostty waybar rofi-wayland hyprlock uwsm grim slurp
 yay -S linux-wallpaperengine
